4e6850ee6c77d1bfef60d987f038e493a61236ff
neatchee <neatchee@gmail.com>
2026-01-22 19:39:55 -0800

    [WIP] Linting

--- utils/core.py
+++ utils/core.py
@@ -3,7 +3,7 @@ from MyClient import AppConfig
 # This is to check if the guild ID is in the config
 def is_guild_in_config(config: AppConfig, guild_id: int):
 
-    guild_ids: dict = config.get("guilds")
+    guild_ids: dict = config.guilds
 
     if guild_id in guild_ids.values():
         return True
@@ -12,7 +12,7 @@ def is_guild_in_config(config: AppConfig, guild_id: int):
     
 def is_rr_message_id_in_config(config: AppConfig, guild_name: str):
 
-    guild_role_message_ids: dict = config.get("guild role messages")
+    guild_role_message_ids: dict = config.guild_role_messages
 
     if guild_name in guild_role_message_ids.keys():
         return True
@@ -21,7 +21,7 @@ def is_rr_message_id_in_config(config: AppConfig, guild_name: str):
 
 def get_channel_id(config: AppConfig, guild_name: str, channel: str):
 
-    channels = config.get("channels").get(channel)
+    channels = config.channels.get(channel)
 
     if channels is None:
         return "Channel not in config"
